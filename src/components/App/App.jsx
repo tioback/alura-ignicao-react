8dc0379a706f69369432aae6832c8044934391da
Renato Pereira Back <renatopb@gmail.com>
2020-09-12 19:22:59 +0000

    Layout base

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AppContainer from '../AppContainer/AppContainer'
+import AppHeader from '../AppHeader/AppHeader'
+import { Wrapper, Container } from './App.styles'
+
+function App() {
+    return (
+        <Wrapper>
+            <Container>
+                <AppHeader />
+                <AppContainer
+                    left={<div style={{ backgroundColor: 'red' }}>produtos disponíveis</div>}
+                    middle={<div style={{ backgroundColor: 'green' }}>sua lista de compras</div>}
+                    right={<div style={{ backgroundColor: 'blue' }}>estatísticas</div>}
+                />
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default App
--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import App from '../components/App/App'
 
 function HomeView () {
     return (
-        <div>
-            todo: home view
-        </div>
+        <App />
     )
 }
 
